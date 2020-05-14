8a7f21e2327eef8adc2449418b0086a5ed8109fb
ThomasFeldman <tfeldman1.31@gmail.com>
2020-05-14 18:54:28 -0500

    chap-6 exercises DONE

---
+++ chap-6_Exercises.js
@@ -0,0 +1,114 @@
+//1
+//Run this code first and examine the error message.
+//Fix the syntax error then run the code again to check your work.
+
+let launchReady = false;
+let fuelLevel = 17000;
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+
+//2
+//This block of code hides two syntax errors.
+
+// Run the code and find the mistakes. 
+// Only ONE error will be flagged at a time. 
+// Fix that ONE problem, and then re-run the code to check yer work. Avoid trying to fix multiple issues at once.
+
+let launchReady = false;
+let crewStatus = true;
+let computerStatus = 'green';
+
+if (crewStatus && computerStatus === 'green'){
+   console.log('Crew & computer cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launchReady = false;
+}
+
+if (launchReady) {
+   console.log("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...");
+   console.log("Fed parrot...");
+   console.log("Ignition...");
+   console.log("Liftoff!");
+} else {
+   console.log("Launch scrubbed.");
+}
+
+//3
+//Run this code first and examine the error message.
+//Pay close attention to any line numbers mentioned in the message - these will help locate and repair the mistake in the code.
+
+let launchReady = false;
+let fuelLevel = 17000;
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+
+//4
+//Now find and fix the runtime error in a longer code sample.
+//Pay close attention to any line numbers mentioned in the message - these will help locate and repair the mistake in the code.
+
+let launchReady = false;
+let fuelLevel = 27000;
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
+
+if (launchReady) {
+   console.log("10, 9, 8...");
+   console.log("Fed parrot...");
+   console.log("6, 5, 4...");
+   console.log("Ignition...");
+   console.log("3, 2, 1...");
+   console.log("Liftoff!");
+} else {
+   console.log("Launch scrubbed.");
+}
+
+//5
+//e) Last piece, the code works now.
+let launchReady = false;
+let fuelReady = false;
+let fuelLevel = 17000;
+let crewStatus = true;
+let computerStatus = 'green';
+
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   fuelReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   fuelReady = false;
+}
+
+if (crewStatus && computerStatus === 'green'){
+   console.log('Crew & computer cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   launchReady = false;
+}
+
+if (launchReady && fuelReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+} else {
+   console.log('Launch scrubbed.');
+}
