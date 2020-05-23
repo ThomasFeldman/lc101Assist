c15c37214b619e86bd1e7922cac568a47382c5d6
ThomasFeldman <tfeldman1.31@gmail.com>
2020-05-23 00:07:09 -0500

    these are actually a little challenging. Will finish by tomorrow.

--- chap-10_Exercises.js
+++ chap-10_Exercises.js
@@ -35,8 +35,97 @@ function makeLine(size){
   }
   makeSquareEasy(6);
 
-  // Part 2 Triangles
-  // Q1   
-  function makeDownwardStairs(height){
-
+  // Part 2 Triangles These are pretty difficult
+// Q1
+function makeDownwardStairs(height){
+    let triArray = [];
+    for(i=1; i<=height; i++){
+      triArray.push('#');
+      console.log(triArray.join(''));
+   }
   }
+  // makeDownwardStairs(5);
+  
+  // Q2
+  function makeSpaceLine(numSpaces, numChars){
+    let finalArray = [];
+    let numSpaceArray = [];
+    let numCharsArray = [];
+  
+    for(i=0;i<numSpaces;i++){
+      numSpaceArray.push('_');
+    }
+    for(i=0;i<numChars;i++){
+      numCharsArray.push('#');
+    }
+    
+    finalArray = finalArray.concat(numSpaceArray,numCharsArray,numSpaceArray);
+  
+    console.log(finalArray.join(''));
+  }
+  //makeSpaceLine(3,5);
+  
+  // Q3
+  //adjusted previous function to make ' ' instead of '_'
+  function makeSpaces(numSpaces, numChars){
+    let finalArray = [];
+    let numSpaceArray = [];
+    let numCharsArray = [];
+  
+    for(i=0;i<numSpaces;i++){
+      numSpaceArray.push(' ');
+    }
+    for(i=0;i<numChars;i++){
+      numCharsArray.push('#');
+    }
+    
+    finalArray = finalArray.concat(numSpaceArray,numCharsArray,numSpaceArray);
+  
+    console.log(finalArray.join(''));
+  }
+  
+  function makeIsoscelesTriangle (height) {
+    let triHashes=1;
+    let triSpaces=height;
+    for(i=0; i<height; i++){
+      makeSpaces(triSpaces/2, triHashes)
+      triSpaces = triSpaces-2;
+      triHashes = triHashes+2;
+    }
+  }
+  //makeIsoscelesTriangle(10);
+  
+  // Q4
+  
+  //make reverse of previous functions
+  
+  function makeReverseSpaces(numSpaces, numChars){
+    let finalArray = [];
+    let numSpaceArray = [];
+    let numCharsArray = [];
+  
+    for(i=0;i<numSpaces;i++){
+      numSpaceArray.push('');
+    }
+    for(i=0;i<numChars;i++){
+      numCharsArray.push('#');
+    }
+    
+    finalArray = finalArray.concat(numSpaceArray,numCharsArray,numSpaceArray);
+  
+    console.log(finalArray.join(''));
+  }
+  
+  
+  function makeReverseIsoscelesTriangle (height) {
+    let triHashes=1;
+    let triSpaces=height;
+    for(i=0; i<height; i++){
+      makeSpaces(triSpaces/2, triHashes)
+      triSpaces = triSpaces-2;
+      triHashes = triHashes+2;
+    }
+  }
+  makeReverseIsoscelesTriangle(3);
+  
+  function makeDiamonds(height){}
