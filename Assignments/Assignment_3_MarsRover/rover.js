ed865e82fe8dbfe4b151116fa033e8a3e1e0ebf7
ThomasFeldman <tfeldman1.31@gmail.com>
2020-07-17 21:40:54 -0500

    chages to rover class

--- Assignments/Assignment_3_MarsRover/rover.js
+++ Assignments/Assignment_3_MarsRover/rover.js
@@ -1,19 +1,38 @@
 class Rover{
-    constructor(position, generatorWatts=110){
-      this.position = position;
-      this.mode = 'NORMAL';
-      this.generatorWatts = generatorWatts;
+  constructor(position, generatorWatts=110){
+    this.position = position;
+    this.mode = 'NORMAL';
+    this.generatorWatts = generatorWatts;
+  }
+
+  receiveMessage(message){
+    this.message = message.name;
+
+    this.results = results.concat(message.commands);
+
+    if(results.contains('MOVE')){
+      this.position = results[1];
+      return '{completed: true}';
+    }else if(results.contains('STATUS_CHECK')){
+      let rover = new Rover(message.position);
+      return `{completed: true, roverStatus: ${rover}}`;
+    }else if(results.contains('MODE_CHANGE')){
+      this.mode = results[0]
+      return '{completed: true}';
+    }else{
+
     }
+  }
   
-  
-    receiveMessage(message){
-        this.message = message.name;
-        this.commands = message.commands;
-        return [message, commands];
-      }
-      
-    }
-    
+}
+
+
+
+// const myCommandObject = new Command('MY_COMMAND_TYPE', 'MY_VALUE')
+
+// myCommandObject.recieveMessage('Hi Mom!');
+
+module.exports = Rover;
     
     // Going to talk to TA tomorrow and hopefully finish this damn project soon
 
