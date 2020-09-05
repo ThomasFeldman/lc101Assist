6e7356531e9f6d0bf957c97afad65ab89aff5b57
ThomasFeldman <tfeldman1.31@gmail.com>
2020-09-05 12:12:18 -0500

    starting to figure out unit tests in java using JUnit4

--- Assignments/Unit 2 Techjobs-OO/src/org/launchcode/techjobs_oo/Tests/org/launchcode/techjobs_oo/JobTest.java
+++ Assignments/Unit 2 Techjobs-OO/src/org/launchcode/techjobs_oo/Tests/org/launchcode/techjobs_oo/JobTest.java
@@ -1,11 +1,15 @@
 package org.launchcode.techjobs_oo;
 
-import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 public class JobTest {
 
+    @Test
+    public void testSettingJobId(){
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertEquals((job1.getId() != job2.getId()), true);
+    }
     @Test
     public void testEquals() {
     }
