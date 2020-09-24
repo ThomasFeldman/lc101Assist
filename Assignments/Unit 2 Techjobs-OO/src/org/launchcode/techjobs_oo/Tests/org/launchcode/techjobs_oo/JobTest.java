4a33d59cf01947e927cba3003508c9ab5c4092ed
ThomasFeldman <tfeldman1.31@gmail.com>
2020-09-23 22:55:07 -0500

    almost done with techjobs_oo

--- Assignments/Unit 2 Techjobs-OO/src/org/launchcode/techjobs_oo/Tests/org/launchcode/techjobs_oo/JobTest.java
+++ Assignments/Unit 2 Techjobs-OO/src/org/launchcode/techjobs_oo/Tests/org/launchcode/techjobs_oo/JobTest.java
@@ -1,7 +1,20 @@
 package org.launchcode.techjobs_oo;
 
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
 public class JobTest {
+
+    Job testJob;
+
+    @Before
+    public void createJobObject(){
+        testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    }
 
     @Test
     public void testSettingJobId(){
@@ -9,8 +22,25 @@ public class JobTest {
         Job job2 = new Job();
         assertEquals((job1.getId() != job2.getId()), true);
     }
-//    @Test
-//    public void testEquals() {
-//
-//    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        assertEquals(testJob.getName(), "Product tester");
+        assertEquals(testJob.getEmployer().getValue(), "ACME");
+        assertEquals(testJob.getLocation().getValue(), "Desert");
+        assertEquals(testJob.getPositionType().getValue(),"Quality control");
+        assertEquals(testJob.getCoreCompetency().getValue(),"Persistence");
+    }
+
+    @Test
+    public void testJobsforEquality(){
+        Job jobA = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job jobB = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals((jobA != jobB), true);
+    }
+
+    @Test
+    public void toStringLine(){
+        assertTrue(testJob.toString().startsWith("\n") && testJob.toString().endsWith("\n"));
+    }
 }
