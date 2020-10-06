57e6bb99421377d6846c3f7acec3e9c902e5ecfc
ThomasFeldman <tfeldman1.31@gmail.com>
2020-10-06 17:44:23 -0500

    working on pirate array exercise

---
+++ Exercises/Pirate_Exercise.js
@@ -0,0 +1,11 @@
+let cargoHold = ['gold', 'not gold'];
+let lootedCargo = [];
+
+let deckMops = function(arr){
+    if (arr === 'gold'){
+        arr = 'not gold';
+    }
+    return arr;
+}
+
+cargoHold.map();
